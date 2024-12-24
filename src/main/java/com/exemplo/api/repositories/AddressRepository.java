24856dfce12c1db458ec3e441d3ad3dcd9485d95
tecoquinho <matheus.lage@icloud.com>
2024-12-24 18:03:49 -0300

    salvando pra trocar de intellij pro ultimate

---
+++ src/main/java/com/exemplo/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.exemplo.api.repositories;
+
+import com.exemplo.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
